ce0adc43109710b5114cba0d07b91da7528c787d
DEENOluwatobi <shodimutobie@gmail.com>
2023-10-20 19:39:52 +0100

    final touches

--- src/components/ProfileMsg.js
+++ src/components/ProfileMsg.js
@@ -21,17 +21,14 @@ const ProfileMsg = ({ profile, setProfile, selectedChat,search ,setSearch }) => 
               duration:0.1
             }
           }}
-        className='w-[410px] h-full border-l bg-black/30 border-slate-700 fixed right-0 overflow-y-scroll'
+        className='w-[410px] h-full border-l bg-black/30 border-slate-700 fixed right-0 overflow-y-scroll z-50'
         >
-          <div className='h-[60px] bg-primary flex items-center gap-6 fixed z-50 top-0 justify-between w-[25rem]'>
+          <div className='h-[60px] bg-primary flex items-center gap-6 fixed z-50 top-0 w-full'>
             <button className='text-icon ml-6' onClick={()=>setProfile(false)}>
               <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" class="" fill="currentColor" enable-background="new 0 0 24 24"><path enable-background="new" d="M19.1,17.2l-5.3-5.3l5.3-5.3l-1.8-1.8L12,10.2L6.7,4.9L4.9,6.6 l5.3,5.3l-5.3,5.3L6.7,19l5.3-5.3l5.3,5.3L19.1,17.2z"></path></svg>
             </button>
             <h1 className='text-white text-lg'>{`${selectedChat.isGroupChat ? "Group" : "Contact"} info`}</h1>
 
-            <button className='text-icon ml-6' onClick={()=>setProfile(false)}>
-              <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" class="" fill="currentColor" enable-background="new 0 0 24 24"><path enable-background="new" d="M19.1,17.2l-5.3-5.3l5.3-5.3l-1.8-1.8L12,10.2L6.7,4.9L4.9,6.6 l5.3,5.3l-5.3,5.3L6.7,19l5.3-5.3l5.3,5.3L19.1,17.2z"></path></svg>
-            </button>
           </div>
           {selectedChat.isGroupChat ? (
             <GroupInfo selectedChat={selectedChat} search={search} setSearch={setSearch} />
--- src/components/SearchMsg.js
+++ src/components/SearchMsg.js
@@ -22,13 +22,13 @@ const SearchMsg = ({ searchMsg, setSearhMsg,selectedChat }) => {
               duration:0.1
             }
           }}
-          className='w-[410px] h-full border-l bg-secondary border-slate-700 fixed right-0'>
+          className='w-[410px] h-full border-l bg-secondary border-slate-700 fixed right-0 z-50'>
 
-          <div className='h-[60px] w-full bg-primary flex items-center gap-6 '>
+          <div className='h-[60px] bg-primary flex items-center gap-6 px-4'>
             <button className='text-icon ml-6' onClick={()=>setSearhMsg(false)}>
-            <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" class="" fill="currentColor" enable-background="new 0 0 24 24"><path enable-background="new" d="M19.1,17.2l-5.3-5.3l5.3-5.3l-1.8-1.8L12,10.2L6.7,4.9L4.9,6.6 l5.3,5.3l-5.3,5.3L6.7,19l5.3-5.3l5.3,5.3L19.1,17.2z"></path></svg>
+              <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" class="" fill="currentColor" enable-background="new 0 0 24 24"><path enable-background="new" d="M19.1,17.2l-5.3-5.3l5.3-5.3l-1.8-1.8L12,10.2L6.7,4.9L4.9,6.6 l5.3,5.3l-5.3,5.3L6.7,19l5.3-5.3l5.3,5.3L19.1,17.2z"></path></svg>
             </button>
-            <h1 className='text-white text-lg'>Search for messages</h1>
+            <h1 className='text-white text-base'>Search messages</h1>
           </div>
           <div className='flex w-full h-[50px] py-[4px] px-3 items-center'>
         <form  action="" className='w-full'>
